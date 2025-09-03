dc2dcb92540c306a7ed835155fb0a1b56d08b4f8
gogeekness <rdeseke@gmail.com>
2025-09-03 08:24:58 +0200

    update to multiplier

--- helloworld.asm
+++ helloworld.asm
@@ -16,6 +16,30 @@ R3:   !byte 0          ; result high
 mulA: !word 0          ; input A (16-bit, unsigned)
 mulB: !word 0          ; input B (16-bit, unsigned)
 
+
+; --- Grid demo constants (Q3.13 fixed-point) ---
+XMIN: !word $C000      ; -2.00 * 8192
+XMAX: !word $1000      ; +0.50 * 8192
+YMIN: !word $D800      ; -1.25 * 8192
+YMAX: !word $2800      ; +1.25 * 8192
+
+
+; Precomputed steps:
+; dx = (XMAX - XMIN)/320 = 20480/320 = 64 = $0040 (exact)
+X_STEP_Q: !word $0040
+X_STEP_R: !word $0000  ; remainder 0
+; dy = (YMAX - YMIN)/200 = 20480/200 = 102 remainder 80 -> $0066, $0050
+Y_STEP_Q: !word $0066
+Y_STEP_R: !word $0050
+
+; Working vars
+x_cur:    !word 0
+y_cur:    !word 0
+cols_lo:  !byte 0
+cols_hi:  !byte 0
+accy_lo:  !byte 0      ; Y remainder accumulator (0..199)
+accy_hi:  !byte 0
+
 ; ===== Code at $C100 =====
 * = $C100
 
@@ -28,21 +52,87 @@ M3 = $FE
 CHROUT = $FFD2
 
 start:
-    ; --- demo inputs: 12345 * 54321 ---
-    lda #<$3039      ; 12345
-    sta mulA
-    lda #>$3039
-    sta mulA+1
+    jsr grid_demo     ; set up X/Y fields and iterate 320x200 once
+    jmp *             ; stop here    jmp *
 
-    lda #<$D431      ; 54321
-    sta mulB
-    lda #>$D431
-    sta mulB+1
 
+; ----------------------------------------------------
+; Grid demo: iterate 320x200 over XMIN..XMAX, YMIN..YMAX (Q3.13)
+; For each (x,y): signed 16x16 multiply and print in same screen spot
+; ----------------------------------------------------
+grid_demo:
+    ; init Y and remainder
+    lda #<YMIN : sta y_cur
+    lda #>YMIN : sta y_cur+1
+    lda #0     : sta accy_lo : sta accy_hi
+
+    ; rows = 200 ($C8)
+    ldy #$C8
+.row:
+    ; X = XMIN at start of each row
+    lda #<XMIN : sta x_cur
+    lda #>XMIN : sta x_cur+1
+
+    ; cols = 320 ($0140)
+    lda #$40 : sta cols_lo
+    lda #$01 : sta cols_hi
+.col:
+    ; Copy (x_cur,y_cur) to mulA/mulB (signed Q3.13)
+    lda x_cur   : sta mulA
+    lda x_cur+1 : sta mulA+1
+    lda y_cur   : sta mulB
+    lda y_cur+1 : sta mulB+1
+
+	; signed multiply
     jsr mul16u       ; (mulA * mulB) -> R3..R0
-    jsr print_all   ; print inputs and product
-    jmp *           ; stop here
 
+	; print location
+    jsr print_spot   ; print inputs and product
+
+    ; x_cur += X_STEP_Q
+    clc
+    lda x_cur   : adc X_STEP_Q   : sta x_cur
+    lda x_cur+1 : adc X_STEP_Q+1 : sta x_cur+1
+
+    ; dec 16-bit cols (dec the hi and lo bytes seperately)
+    dec cols_lo
+    bne .col_cont
+    dec cols_hi
+.col_cont:
+    lda cols_lo
+    ora cols_hi
+    bne .col
+    ; End of row: y_cur += Y_STEP_Q
+
+    clc
+    lda y_cur   : adc Y_STEP_Q   : sta y_cur
+    lda y_cur+1 : adc Y_STEP_Q+1 : sta y_cur+1
+
+    ; accy += Y_STEP_R
+    clc
+    lda accy_lo : adc #<Y_STEP_R : sta accy_lo
+    lda accy_hi : adc #>Y_STEP_R : sta accy_hi
+
+    ; if accy >= 200 ($00C8), subtract 200 and y_cur += 1
+    lda accy_hi
+    bne .yfix
+    lda accy_lo
+    cmp #$C8
+    bcc .ynofix
+.yfix:    
+    sec
+    lda accy_lo : sbc #$C8 : sta accy_lo
+    lda accy_hi : sbc #$00 : sta accy_hi
+    inc y_cur
+    bne .ynofix
+    inc y_cur+1
+.ynofix:
+    dey
+    bne .rows_done
+    jmp .row
+
+.rows_done:
+	rts
 
 ; ----------------------------------------------------
 ; mul16u: unsigned 16×16 -> 32
@@ -110,7 +200,7 @@ print_all:
     ; Print B
     ldx #0
 .pb1 lda msgB,x
-    beq .pb1done
+    beq .pb1done 
     jsr CHROUT
     inx
     bne .pb1
